02425255fb14deda5d1c4c2eb0a6d8ff715bafe6
gkiss <gk@20shots.co.uk>
2024-04-30 20:37:32 +0100

    init leetcode with first task.

---
+++ src/main/java/gk/uk/learning/App.java
@@ -0,0 +1,13 @@
+package gk.uk.learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/gk/uk/learning/easy/merge_sorted_array/MergeSortedArrayTest.java
@@ -0,0 +1,70 @@
+package gk.uk.learning.easy.merge_sorted_array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+
+public class MergeSortedArrayTest {
+
+    @Test
+    public void testMergeTwoNonEmptyArrays() {
+        // Arrange
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        int[] expected = {1, 2, 2, 3, 5, 6};
+
+        // Act
+        mergeSortedArray.merge(nums1, 3, nums2, 3);
+
+        // Assert
+        assertArrayEquals(expected, nums1);
+    }
+
+    @Test
+    public void testMergeWithEmptySecondArray() {
+        // given
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] nums1 = {1, 2, 3};
+        int[] nums2 = {};
+        int[] expected = {1, 2, 3};
+
+        // when
+        mergeSortedArray.merge(nums1, 3, nums2, 0);
+
+        // then
+        assertArrayEquals(expected, nums1);
+    }
+
+    @Test
+    public void testMergeWithEmptyFirstArray() {
+        // given
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] nums1 = {0};
+        int[] nums2 = {1};
+        int[] expected = {1};
+
+        // when
+        mergeSortedArray.merge(nums1, 0, nums2, 1);
+
+        // then
+        assertArrayEquals(expected, nums1);
+    }
+
+    @Test
+    public void testMergeWhenFirstArrayHasOnlyZeros() {
+        // given
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] nums1 = {0, 0, 0};
+        int[] nums2 = {1, 2, 3};
+        int[] expected = {1, 2, 3};
+
+        // when
+        mergeSortedArray.merge(nums1, 0, nums2, 3);
+
+        // then
+        assertArrayEquals(expected, nums1);
+    }
+}
+
